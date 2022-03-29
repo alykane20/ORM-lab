7d1d7023f44d9b22d90ab2fcde30ade2eb0f842c
Aly Kane <Aly@macbook-air.myfiosgateway.com>
2022-03-29 17:05:38 -0400

    Completed problems 5-7

--- school_db/views.py
+++ school_db/views.py
@@ -255,9 +255,15 @@ SELECT COUNT(*) AS `__count`
 # NOTE every time you execute this function a duplicate student will be created with a different primary key number
 def problem_five(request):
 
+  student = Student.objects.create(first_name ='Aly', last_name = 'Kane', year = 2022 , gpa = 4.0)
+  print(f"""Id:{student.id}
+Full Name: {student.first_name} {student.last_name}
+Year:{student.year}
+GPA:{student.gpa}""")
 
 
-    return complete(request)
+
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -288,13 +294,21 @@ VALUES ('Kyle', 'Harwood', 2022, 3.0)
 # Then query the studets table to get that student by their id
 # Print the new student's id, full name, and gpa to the terminal
 def problem_six(request):
+  student_id = 13
+  Student.objects.filter(pk=student_id).update(gpa=3.5)
+  student = Student.objects.get(pk=student_id)
+  print(f"""
+  Id: {student.id}
+  Full Name:{student.first_name} {student.last_name}
+  GPA:{student.gpa}""")
+
     
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
+  
 
 
 
-    return complete(request)
+  return complete(request)
 
 
 # Supporting Query Method Documentation:
@@ -339,8 +353,8 @@ LIMIT 21
 def problem_seven(request):
 
     # Make sure to set this equal to the primary key of the row you just created!
-    student_id = 11
-
+    student_id = 13
+    Student.objects.filter(pk=student_id).delete()
 
     try:
         student = Student.objects.get(pk=student_id)
